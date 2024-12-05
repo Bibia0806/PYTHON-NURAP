3477e44f330479d291cbf00001a3e76137e67d6b
Bibia0806 <beatrizbarboza011@gmail.com>
2024-12-05 11:32:47 -0300

    Add files via upload

---
+++ Aula02/aula2.py
@@ -0,0 +1,18 @@
+print('quero um doce e ferias do trabalho')
+
+nome_usuario = 'Beatriz' # tipo-> str = string 
+
+idade_usuario = 17  # tipo -> int = interger
+
+altura_usuario = 1.58 # tipo -> float = flutuante
+
+maior_idade = False # tipo -> boll= boolean
+
+print('Seja Bem-Vindo', nome_usuario)
+
+print('sua idade é', idade_usuario, 'anos')
+
+print('voce tem ', altura_usuario, 'metros')
+
+print('voce é maio de idade?', maior_idade)
+
